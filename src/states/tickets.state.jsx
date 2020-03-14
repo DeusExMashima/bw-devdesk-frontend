d8622823a9d3a616a7b2c8d3dff0cf135d42b980
Mashima <52222646+zimashima@users.noreply.github.com>
2020-03-14 19:18:20 -0400

    added state folders and initialized state

---
+++ src/states/tickets.state.jsx
@@ -0,0 +1,19 @@
+export const singleTicketState = {
+    id: null,
+    title: "",
+    status: "",
+    description: "",
+    asker: "",
+    assignee: "",
+    attempedSolutions: "",
+    category: "",
+    createdAt: "",
+    solution: {
+        solutionId: null,
+        body: "",
+        answerer: "",
+        createdAt: ""
+    }
+}
+
+export const tickets = []
